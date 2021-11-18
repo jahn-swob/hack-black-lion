21453093dad42f5a5a03393c5b15deeef02a7f32
mmeraz <mmeraz@crateandbarrel.com>
2021-11-18 15:52:37 -0600

    Made a simple search component preview screen

--- demo/search/src/Standalone.jsx
+++ demo/search/src/Standalone.jsx
@@ -4,27 +4,12 @@ import 'team-shell/BaseStyles';
 import "./styles/index.css";
 
 import { products } from 'team-landing/MockedProducts'
+import Search from './federated/Search'
 
 const Standalone = () => {
   return (
     <div className="site">
-      <div className="checkoutPage">
-        <h2>Checkout (standalone)</h2>
-        <div className="checkoutList">
-        {
-        products.map((item, index) => {
-          return (
-            <div className="checkoutProduct" key={item.name}>
-              <div className="checkoutTitle">{item.name}</div>
-          <div className="checkoutDescription">{item.description}</div>
-              <div className="checkoutPrice">${item.price}</div>
-            </div>
-            )
-        })
-        }
-        <div className="checkoutTotal">{products.reduce((a, b) => (a + b.price), 0)}</div>
-        </div>
-      </div>
+      <Search />
     </div>
   )
 };
