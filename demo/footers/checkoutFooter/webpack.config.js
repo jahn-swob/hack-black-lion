280b2ba4ffbab4e1e23c347698f42864e5faf75e
Nguyen <hnguyen@cb.crateandbarrel.com>
2021-11-18 16:07:28 -0600

    added checkout footer to products standalone

--- demo/footers/checkoutFooter/webpack.config.js
+++ demo/footers/checkoutFooter/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 3001,
+    port: 3004,
   },
 
   module: {
--- demo/landing/src/federated/Landing.jsx
+++ demo/landing/src/federated/Landing.jsx
@@ -3,7 +3,6 @@ import "./../styles/index.css";
 
 import { products } from 'team-landing/MockedProducts';
 const BuyButton = React.lazy(() => import("team-checkout/BuyButton"));
-const CheckoutFooter = React.lazy(() => import("team-checkout-footer/CheckoutFooter"));
 
 const Landing = () => {
   return (
@@ -17,7 +16,6 @@ const Landing = () => {
           <div style={{marginBottom: 20}}>{item.name}</div>
           <React.Suspense fallback={<button>buy</button>}>
             <BuyButton payload={{name: item.name, price: item.price, description: item.description}}>BUY - ${item.price}</BuyButton>
-            <CheckoutFooter />
           </React.Suspense>
           </div>
           )
