1ba24e9f6bd4604e476786ca3e3f8cee76c93e0b
mmeraz <mmeraz@crateandbarrel.com>
2021-11-18 22:25:20 -0600

    Code clean-up

--- demo/header/src/Header.jsx
+++ demo/header/src/Header.jsx
@@ -4,20 +4,20 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link, withRouter } from "react-router-dom";
 import "./styles/index.css";
 
-const Landing = React.lazy(() => import("team-landing/Landing"));
-const Checkout = React.lazy(() => import("team-checkout/Checkout"));
 const Cart = React.lazy(() => import("team-checkout/Cart"));
 const Search = React.lazy(() => import("team-search/Search"));
 
 const NavLinks = ({location}) => {
   return (
-    <div className="links">
-      <a href="/">
+    <div className="nav-links">
+      <Link to="/">
         Products
-      </a>
-      <a href="/checkout" >
+      </Link>
+      { location.pathname !== '/checkout' && (
+      <Link to="/checkout" >
         Checkout
-      </a>
+      </Link>
+      )}
     </div>
   )
 }
@@ -28,7 +28,7 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <h2>Black Lion e-commerce</h2>
+        <h1>Black Lion</h1>
         <LinksWrapper />
         <div className="ecommerce-nav">
           <React.Suspense fallback={<div/>}>
