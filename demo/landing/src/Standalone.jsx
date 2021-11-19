e8d82674c7c726300d76548d201c1a96f95d9790
mmeraz <mmeraz@crateandbarrel.com>
2021-11-18 22:27:09 -0600

    Made product card separate component

--- demo/landing/src/Standalone.jsx
+++ demo/landing/src/Standalone.jsx
@@ -5,28 +5,22 @@ import { products } from 'team-landing/MockedProducts';
 import "./styles/index.css";
 
 const BuyButton = React.lazy(() => import("team-checkout/BuyButton"));
+import Product from "./federated/Product";
 
 const Main = () => {
   return (
-    <div className="site">
       <div className="landingPage">
         <h2>Products (standalone)</h2>
         <div className="productList">
         {
         products.map((item, index) => {
           return (
-            <div className="product" key={item.name}>
-            <div style={{marginBottom: 20}}>{item.name}</div>
-            <React.Suspense fallback={<button>buy</button>}>
-            <BuyButton payload={{name: item.name, price: item.price, description: item.description}}>BUY - ${item.price}</BuyButton>
-            </React.Suspense>
-            </div>
+              <Product data={item} key={item.name} />
             )
         })
         }
         </div>
       </div>
-    </div>
   )
 };
 
--- demo/landing/src/federated/Landing.jsx
+++ demo/landing/src/federated/Landing.jsx
@@ -3,21 +3,19 @@ import "./../styles/index.css";
 
 import { products } from 'team-landing/MockedProducts';
 const BuyButton = React.lazy(() => import("team-checkout/BuyButton"));
+import Product from './Product';
 
 const Landing = () => {
   return (
     <div className="landingPage">
-      <h2>Products</h2>
+      <h2>Our Products</h2>
       <div className="productList">
       {
       products.map((item, index) => {
         return (
-          <div className="product" key={item.name}>
-          <div style={{marginBottom: 20}}>{item.name}</div>
-          <React.Suspense fallback={<button>buy</button>}>
-            <BuyButton payload={{name: item.name, price: item.price, description: item.description}}>BUY - ${item.price}</BuyButton>
-          </React.Suspense>
-          </div>
+          <>
+          <Product data={ item } key={item.name}/>
+          </>
           )
       })
       }
