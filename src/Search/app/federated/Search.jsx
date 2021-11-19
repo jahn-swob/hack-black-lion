8ad359b8cf87ac5eb45176029374279c7b9e8129
EmanCrateAndBarrel <94140356+EmanCrateAndBarrel@users.noreply.github.com>
2021-11-19 11:53:07 -0600

    changed the search to how it should be

--- src/Search/app/federated/Search.jsx
+++ src/Search/app/federated/Search.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import "./../styles/index.css";
 
-export const Search = () => {
-  return (
-    <div className="search-bar-container">
-      <input
-        placeholder="Search products..."
-        type="text"
-        className="search-input"
-        onKeyDown={handleChange} />
+const Search = () => {
+    return (
+        <div className="search-bar-container">
+            <input
+                placeholder="Search products..."
+                type="text"
+                className="search-input"
+                onKeyDown={handleChange} />
 
-      <button className="search-button" type="button" onClick={handleSubmit}>Search</button>
-    </div>
-  )
+            <button className="search-button" type="button" onClick={handleSubmit}>Search</button>
+        </div>
+    )
 };
 
 const handleChange = e => {
@@ -21,30 +21,53 @@ const handleChange = e => {
     var userInput = string += JSON.stringify(e.target.value);
 
     if (e.keyCode === 13) {
-        handleSubmit(userInput);
+        handleSubmit(userInput.toString());
     }
 }
 
 const handleSubmit = userInput => {
-    console.log("userInput", userInput)
-
-    searchProduct(userInput);
+    // fetch(`https://localhost:55006/Search?q=${userInput.toLowerCase()}`)
+    fetch("https://localhost:7175/Search?q=plates")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log("result", result); // we have the result!!
+                // searchProduct(result);
+                setResults(result);
+            },
+            (error) => {
+                console.error("ERROR WITH SEARCH RESULTS: ", error);
+            }
+        )
 }
 
-const searchProduct = userInput => dispatch => {
-  dispatch({
-    type: "search",
-    userInput: userInput
-  });
+// const SearchButton = ({ payload, setResults, children }) => (
+//   // <button className="checkoutButton" onClick={() => addToCart(payload)}>{children}</button>
+//   <button className="search-button" onClick={() => setResults(payload)}>Search</button>
+//   );
+
+function searchProduct(payload) {
+    return { type: 'landing/loadproducts', payload }
 }
+
+export default connect(null, (dispatch) => ({
+    setResults: (payload) => dispatch(searchProduct(payload))
+}))(Search);
+
+// const searchProduct = userInput => dispatch => {
+//   dispatch({
+//     type: "landing/loadproducts",
+//     userInput: userInput
+//   });
+// }
 
 // const SearchButton = ({ payload, addToCart, children }) => (
 //     <button className="checkoutButton" onClick={() => addToCart(payload)}>{children}</button>
 //     );
 
-const mapStateToPros = state => ({ items: state.items });
+// const mapStateToPros = state => ({ items: state.items });
 
-// export default connect(null, (dispatch) => ({
-//   addToCart: (payload) => dispatch(buyItem(payload))
-// }))(BuyButton);
-export default connect(mapStateToPros, {})(Search);
+// // export default connect(null, (dispatch) => ({
+// //   addToCart: (payload) => dispatch(buyItem(payload))
+// // }))(BuyButton);
+// export default connect(mapStateToPros, {})(Search);
